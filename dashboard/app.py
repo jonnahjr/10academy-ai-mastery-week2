aa610a471d0b43d8d9f34f1e1c2b182c9d5712ab
jonnahjr <pronivamusic14@gmail.com>
2024-12-19 12:09:59 +0300

    import sys and os

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 from dashboard.pages.user_overview import display_user_overview
 from dashboard.pages.engagement import display_engagement
